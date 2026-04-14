794426aa1a1caa8840255caf7b833d45a3fffd74
user <adm713934@yandex.ru>
2026-04-14 14:14:29 +0300

    format readme

--- sql/1.1_products.sql
+++ sql/1.1_products.sql
@@ -1,7 +1,7 @@
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     category_id INTEGER NOT NULL REFERENCES categories(id) ON DELETE RESTRICT,
-    name VARCHAR(150) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     price DECIMAL(10, 2) NOT NULL CHECK (price >= 0),
     stock_quantity INTEGER NOT NULL DEFAULT 0 CHECK (stock_quantity >= 0)
 );
